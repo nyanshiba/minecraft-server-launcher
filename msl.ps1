5d84b9e554e36ff4c38370055c9be313492f92d6
nyanshiba <31783332+nyanshiba@users.noreply.github.com>
2021-12-15 22:42:31 +0900

    色いらん

--- msl.ps1
+++ msl.ps1
@@ -335,7 +335,7 @@ foreach ($profile in ($Settings.Profiles | Where-Object {$_.Name -in $Name}))
         }
     }
 
-    $profile | Select-Object -Property Name, Rcon, File, Arg, Dir, Window
+    $profile | Select-Object -Property Name, Rcon, File, Arg, Dir | Out-String
 
     #起動・停止関数を呼ぶ
     switch ($Action)
